0a5717ed4fb9fbd11dc9bf79e63be95f3b178104
minakdr <mina125.ak@gmail.com>
2025-01-05 16:12:03 +0100

    l

---
+++ src/class1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Class1 extends React.Component{
+    render(){
+        return (
+            <h1>this is a {this.props.type} component</h1>
+        )
+    }
+
+
+}
+
+export default Class1;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import Class1 from './class1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <App />
+    <Class1 type='class' />
 
 );
 
